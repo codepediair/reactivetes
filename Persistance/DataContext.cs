65babb4124dc8bbdafebcc9672c064869303f2f7
Mahdi <you@example.com>
2021-09-02 13:25:59 +0430

    initialized Reactivites project

---
+++ Persistance/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistance
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) :base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+                .HasData(
+                    new Value {Id = 1, Name = "VAL01"},
+                    new Value {Id = 2, Name = "VAL02"},
+                    new Value {Id = 3, Name = "VAL03"},
+                    new Value {Id = 4, Name = "VAL04"}
+                );
+        }
+    }
+}
